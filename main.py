69cdeed6a8f354f17bab513cc136b777df3d8d64
Edward Barak <edward.barak@gmail.com>
2020-12-29 15:02:07 -0800

    created openUrls function

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 import requests
-from bs4 import beautifulsoup as bs
+import sys
+import re
+from bs4 import BeautifulSoup as bs
 
+# CONFIG
 jobTitleSelector = ''
 companySelector = ''
+
+# Functions
+def openUrls():
+    fname = sys.argv[1]
+    with open(fname, 'r') as f:
+        urls = f.read()
+        f.close()
+    
+    urls = urls.split('\n')
+    urls.remove('')
+
+    return urls
+
+
+
+if __name__ == "__main__":
+    # ptn = re.compile('^(.*?)\..*')
+    # fname = ptn.findall(sys.argv[1])[0]
+
+    # print(openUrls())
+
 
 # open text file with URLs
 # loop through each URL and grab the following:
