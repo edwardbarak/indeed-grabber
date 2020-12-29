da8af5bc447f3e98aee06ce211c0386ae1f9de5c
Edward Barak <edward.barak@gmail.com>
2020-12-29 14:33:42 -0800

    structure code

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import beautifulsoup as bs
+
+jobTitleSelector = ''
+companySelector = ''
+
+# open text file with URLs
+# loop through each URL and grab the following:
+    # Job title
+    # Company name
+# Create numbered list of jobs
