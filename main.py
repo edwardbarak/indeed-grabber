44521df921398bf9f781828f276c128dd3ddde5b
Edward Barak <edward.barak@gmail.com>
2020-12-29 16:18:47 -0800

    xml selectors

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import re
 from bs4 import BeautifulSoup as bs
 
 # CONFIG
-jobTitleSelector = ''
-companySelector = ''
+jobTitleSelector = '/html/body/div[1]/div[2]/div[3]/div/div/div[1]/div[1]/div/div[3]/div[1]/div[1]/h1/text()'
+companySelector = '/html/body/div[1]/div[2]/div[3]/div/div/div[1]/div[1]/div/div[3]/div[1]/div[2]/div/div/div/div[1]/a/text()'
 
 # Functions
 def openUrls():
@@ -19,17 +19,14 @@ def openUrls():
 
     return urls
 
-
-
 if __name__ == "__main__":
     # ptn = re.compile('^(.*?)\..*')
     # fname = ptn.findall(sys.argv[1])[0]
 
-    # print(openUrls())
-
-
-# open text file with URLs
-# loop through each URL and grab the following:
-    # Job title
-    # Company name
-# Create numbered list of jobs
+    # open text file with URLs
+    urls = openUrls()
+    
+    # loop through each URL and grab the following:
+        # Job title
+        # Company name
+    # Create numbered list of jobs
